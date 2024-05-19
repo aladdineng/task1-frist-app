1ce9afa4ed43a585eeacdf74934cae39fe7af90a
aladdin <aladdin.enbeh@gmail.com>
2024-05-19 19:30:01 +0300

    done

---
+++ src/App.js
@@ -0,0 +1,19 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      
+      <div style={{background:"#219ebc"}}>
+<h1>Coockies and Beyond</h1>
+<h4>Where cookie maniacs gather</h4>
+<img src='https://www.leafie.co.uk/wp-content/uploads/2022/01/cookies-cannabis-store.jpeg'></img>
+
+      </div>
+     
+    </div>
+  );
+}
+
+export default App;
